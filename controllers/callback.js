2e6e160ad2a7858f367b331320e08e18d7f4db85
rchaves <rogeriocfj@rchaves.local>
2011-12-26 23:38:47 -0200

    facebook credits

--- controllers/callback.js
+++ controllers/callback.js
@@ -1,9 +1,20 @@
-app.all('/callback', function(req, res) {
-	console.log("===================================");
-	console.log(req.query);
-	console.log("===================================");
-	console.log(req.body);
-	console.log("===================================");
-	console.log(req.param('method'));
-	res.send('teste');
+var qs = require('querystring');
+
+app.all('/callback', function(request, response) {
+	
+	if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            var data = qs.parse(body);
+			console.log("==========");
+			console.log(data);
+			console.log("===========");
+			response.send(data);
+
+        });
+    }
+
 });
