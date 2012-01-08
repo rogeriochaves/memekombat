16e7bb93e893df4dbe51ffd050b07e5120d6fac5
rchaves <rogeriocfj@rchaves.local>
2012-01-08 19:33:45 -0200

    friends_using limit

--- controllers/index.js
+++ controllers/index.js
@@ -10,6 +10,7 @@ var render_index = function(req, res, session, novo_personagem){
 		
 		// Amigos que jogam
 		amigos_usando(req, res, function(friends_using){
+			friends_using.splice(0, 6);
 			/*result.forEach(function(friend) {
 				socket_manager.send(socket_id, 'friend_using_app', friend);
 			});*/
