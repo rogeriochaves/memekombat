b1c7d4f3d06b7c68ef98a62135de96c733c9404b
Rogério Chaves <rogeriocfj+github@gmail.com>
2016-07-24 12:11:11 -0300

    Remove fql call from inicio

--- controllers/inicio.js
+++ controllers/inicio.js
@@ -15,10 +15,7 @@ app.all('/inicio', function(request, response) {
 		facebook.getSessionByAccessToken(token)(function(session) { // usuário autenticado
 
 			// requisita os amigos que já estão jogando
-			session.restCall('fql.query', {
-				query: 'SELECT uid, name, is_app_user FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1 ORDER BY rand()',
-				format: 'json'
-			})(function(amigos) {
+			amigos_usando(request, response, function(amigos){
 
 				var user = request.session.auth.facebook.user;
 		        // renderiza a página
@@ -26,8 +23,8 @@ app.all('/inicio', function(request, response) {
 		          layout:   false,
 		          token:    token,
 		          user:     user,
-		 		  amigos: amigos,
-				  portugues: (user.locale && user.locale.indexOf('pt') >= 0),
+		 		  		amigos: amigos,
+				  		portugues: (user.locale && user.locale.indexOf('pt') >= 0),
 		          home:     method + '://' + request.headers.host + '/',
 		          redirect: method + '://' + request.headers.host + request.url,
 		          //socket_id: socket_id
